27d262e0071ad98850625c40e5ae1b3afb232074
Marek Markiewka <marek.markiewka@cloudogu.com>
2021-02-08 08:55:34 +0100

    syntax bugfixing
    Signed-off-by: Marek Markiewka <marek.markiewka@cloudogu.com>

--- vars/deployViaGitops.groovy
+++ vars/deployViaGitops.groovy
@@ -5,9 +5,9 @@ String getConfigDir() { '.config'}
 String getHelmImage() { 'ghcr.io/cloudogu/helm:3.4.1-1'}
 String getYamlLintImage() { 'cytopia/yamllint:1.25' }
 
-void call(Map gitopsConfig) {
+void call(def script, Map gitopsConfig) {
   cesBuildLib = initCesBuildLib(gitopsConfig.cesBuildLibRepo, gitopsConfig.cesBuildLibVersion)
-  deploy(gitopsConfig)
+  deploy(script, gitopsConfig)
 }
   
 private initCesBuildLib(cesBuildLibRepo, cesBuildLibVersion) {
@@ -16,11 +16,11 @@ private initCesBuildLib(cesBuildLibRepo, cesBuildLibVersion) {
   ).com.cloudogu.ces.cesbuildlib
 }
 
-private void deploy(Map gitopsConfig) {
+private void deploy(def script, Map gitopsConfig) {
 
   echo "in deploy method"
 
-  def git = cesBuildLib.Git.new(script, gitopsconfig.scmmCredentialsId)
+  def git = cesBuildLib.Git.new(this, gitopsConfig.scmmCredentialsId)
   def changesOnGitOpsRepo = ''
 
   // Query and store info about application repo before cloning into gitops repo
@@ -43,14 +43,14 @@ private void deploy(Map gitopsConfig) {
 
       gitopsConfig.stages.each{ stage, config ->
         //checkout the main_branch before creating a new stage_branch. so it won't be branched off of an already checked out stage_branch
-        git.checkoutOrCreate(mainBranch)
+        git.checkoutOrCreate(gitopsConfig.mainBranch)
 
         if(config.deployDirectly) {
-          allRepoChanges += createApplicationForStageAndPushToBranch stage as String, mainBranch, applicationRepo, git, gitopsConfig
+          allRepoChanges += createApplicationForStageAndPushToBranch script, stage as String, gitopsConfig.mainBranch, applicationRepo, git, gitopsConfig
         } else {
           String stageBranch = "${stage}_${application}"
           git.checkoutOrCreate(stageBranch)
-          String repoChanges = createApplicationForStageAndPushToBranch stage as String, stageBranch, applicationRepo, git, gitopsConfig
+          String repoChanges = createApplicationForStageAndPushToBranch script, stage as String, stageBranch, applicationRepo, git, gitopsConfig
           if(repoChanges) {
             createPullRequest(gitopsConfig, stage as String, stageBranch)
             allRepoChanges += repoChanges
@@ -64,107 +64,111 @@ private void deploy(Map gitopsConfig) {
   }
 
   currentBuild.description = createBuildDescription(changesOnGitOpsRepo, gitopsConfig.imageName)
+}
+
+
+private String createApplicationForStageAndPushToBranch(def script, String stage, String branch, GitRepo applicationRepo, def git, Map gitopsConfig) {
+
+  String commitPrefix = "[${stage}] "
+
+  sh "mkdir -p ${stage}/${gitopsConfig.application}/"
+  sh "mkdir -p ${configDir}/"
+  // copy extra resources like sealed secrets
+  echo "Copying k8s payload from application repo to gitOps Repo: 'k8s/${stage}/*' to '${stage}/${gitopsConfig.application}'"
+  sh "cp ${env.WORKSPACE}/k8s/${stage}/* ${stage}/${gitopsConfig.application}/ || true"
+  sh "cp ${env.WORKSPACE}/*.yamllint.yaml ${configDir}/ || true"
+
+  // TODO user decides if validation is necessary
+  validateK8sRessources(script, "${stage}/${gitopsConfig.application}/", k8sVersion)
+  validateYamlResources(script, "${configDir}/config.yamllint.yaml", "${stage}/${gitopsConfig.application}/")
+
+  gitopsConfig.updateImages.each {
+    updateImageVersion("${stage}/${gitopsConfig.application}/${it['deploymentFilename']}", it['containerName'], it['imageName'])
   }
 
-  private String createApplicationForStageAndPushToBranch(String stage, String branch, GitRepo applicationRepo, def git, Map gitopsConfig) {
+  git.add('.')
+  if (git.areChangesStagedForCommit()) {
+    git.commit(commitPrefix + createApplicationCommitMessage(git, applicationRepo), applicationRepo.authorName, applicationRepo.authorEmail)
 
-    String commitPrefix = "[${stage}] "
-
-    sh "mkdir -p ${stage}/${application}/"
-    sh "mkdir -p ${configDir}/"
-    // copy extra resources like sealed secrets
-    echo "Copying k8s payload from application repo to gitOps Repo: 'k8s/${stage}/*' to '${stage}/${application}'"
-    sh "cp ${env.WORKSPACE}/k8s/${stage}/* ${stage}/${application}/ || true"
-    sh "cp ${env.WORKSPACE}/*.yamllint.yaml ${configDir}/ || true"
-
-    // TODO user decides if validation is necessary
-    validateK8sRessources("${stage}/${application}/", k8sVersion)
-    validateYamlResources("${configDir}/config.yamllint.yaml", "${stage}/${application}/")
-
-    gitopsConfig.updateImages.each {
-      updateImageVersion("${stage}/${application}/${it['deploymentFilename']}", it['containerName'], it['imageName'])
-    }
-
-    git.add('.')
-    if (git.areChangesStagedForCommit()) {
-      git.commit(commitPrefix + createApplicationCommitMessage(git, applicationRepo), applicationRepo.authorName, applicationRepo.authorEmail)
-
-      // If some else pushes between the pull above and this push, the build will fail.
-      // So we pull if push fails and try again
-      git.pushAndPullOnFailure("origin ${branch}")
-      return "${stage} (${git.commitHashShort})"
-    } else {
-      echo "No changes on gitOps repo for ${stage} (branch: ${branch}). Not committing or pushing."
-      return ''
-    }
-  }
-
-  private String aggregateChangesOnGitOpsRepo(changes) {
-    // Remove empty
-    (changes - '')
-    // and concat into string
-            .join('; ')
-  }
-
-  private String createApplicationCommitMessage(def git, def applicationRepo) {
-    String issueIds =  (applicationRepo.commitMessage =~ /#\d*/).collect { "${it} " }.join('')
-
-    String[] urlSplit = applicationRepo.repositoryUrl.split('/')
-    def repoNamespace = urlSplit[-2]
-    def repoName = urlSplit[-1]
-    String message = "${issueIds}${repoNamespace}/${repoName}@${applicationRepo.commitHash}"
-
-    return message
-  }
-
-  private void createPullRequest(Map gitopsConfig, String stage, String sourceBranch) {
-
-    withCredentials([usernamePassword(credentialsId: gitopsConfig.scmmCredentialsId, passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USER')]) {
-
-      String script =
-              'curl -s -o /dev/null -w "%{http_code}" ' +
-                      "-u ${GIT_USER}:${GIT_PASSWORD} " +
-                      '-H "Content-Type: application/vnd.scmm-pullRequest+json;v=2" ' +
-                      '--data \'{"title": "created by service ' + application + ' for stage ' + stage + '", "source": "' + sourceBranch + '", "target": "' + mainBranch + '"}\' ' +
-                      gitopsConfig.scmmPullRequestUrl
-
-      // For debugging the quotation of the shell script, just do: echo script
-      String http_code = sh returnStdout: true, script: script
-
-      // At this point we could write a mail to the last committer that his commit triggered a new or updated GitOps PR
-
-      echo "http_code: ${http_code}"
-      // PR exists if we get 409
-      if (http_code != "201" && http_code != "409") {
-        unstable 'Could not create pull request'
-      }
-    }
-  }
-
-  private void updateImageVersion(String deploymentFilePath, String containerName, String newImageTag) {
-    def data = readYaml file: deploymentFilePath
-    def containers = data.spec.template.spec.containers
-    def updateContainer = containers.find {it.name == containerName}
-    updateContainer.image = newImageTag
-    writeYaml file: deploymentFilePath, data: data, overwrite: true
+    // If some else pushes between the pull above and this push, the build will fail.
+    // So we pull if push fails and try again
+    git.pushAndPullOnFailure("origin ${branch}")
+    return "${stage} (${git.commitHashShort})"
+  } else {
+    echo "No changes on gitOps repo for ${stage} (branch: ${branch}). Not committing or pushing."
+    return ''
   }
 }
 
+private String aggregateChangesOnGitOpsRepo(changes) {
+  // Remove empty
+  (changes - '')
+  // and concat into string
+          .join('; ')
+}
+
+private String createApplicationCommitMessage(def git, def applicationRepo) {
+  String issueIds =  (applicationRepo.commitMessage =~ /#\d*/).collect { "${it} " }.join('')
+
+  String[] urlSplit = applicationRepo.repositoryUrl.split('/')
+  def repoNamespace = urlSplit[-2]
+  def repoName = urlSplit[-1]
+  String message = "${issueIds}${repoNamespace}/${repoName}@${applicationRepo.commitHash}"
+
+  return message
+}
+
+private void createPullRequest(Map gitopsConfig, String stage, String sourceBranch) {
+
+  withCredentials([usernamePassword(credentialsId: gitopsConfig.scmmCredentialsId, passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USER')]) {
+
+    String script =
+            'curl -s -o /dev/null -w "%{http_code}" ' +
+                    "-u ${GIT_USER}:${GIT_PASSWORD} " +
+                    '-H "Content-Type: application/vnd.scmm-pullRequest+json;v=2" ' +
+                    '--data \'{"title": "created by service ' + application + ' for stage ' + stage + '", "source": "' + sourceBranch + '", "target": "' + gitopsConfig.mainBranch + '"}\' ' +
+                    gitopsConfig.scmmPullRequestUrl
+
+    // For debugging the quotation of the shell script, just do: echo script
+    String http_code = sh returnStdout: true, script: script
+
+    // At this point we could write a mail to the last committer that his commit triggered a new or updated GitOps PR
+
+    echo "http_code: ${http_code}"
+    // PR exists if we get 409
+    if (http_code != "201" && http_code != "409") {
+      unstable 'Could not create pull request'
+    }
+  }
+}
+
+
+private void updateImageVersion(String deploymentFilePath, String containerName, String newImageTag) {
+  def data = readYaml file: deploymentFilePath
+  def containers = data.spec.template.spec.containers
+  def updateContainer = containers.find {it.name == containerName}
+  updateContainer.image = newImageTag
+  writeYaml file: deploymentFilePath, data: data, overwrite: true
+}
+
 // Validates all yaml-resources within the target-directory against the specs of the given k8s version
-private void validateK8sRessources(String targetDirectory, String k8sVersion) {
-  withDockerImage(helmImage) {
+private void validateK8sRessources(def script, String targetDirectory, String k8sVersion) {
+  withDockerImage(script, helmImage) {
     sh "kubeval -d ${targetDirectory} -v ${k8sVersion} --strict"
   }
 }
 
-private void validateYamlResources(String configFile, String targetDirectory) {
-  withDockerImage(yamlLintImage) {
+private void validateYamlResources(def script, String configFile, String targetDirectory) {
+  withDockerImage(script, yamlLintImage) {
     sh "yamllint -c ${configFile} ${targetDirectory}"
   }
 }
 
-private void withDockerImage(String image, Closure body) {
-  cesBuildLib.Docker.new(this).image(image)
+private void withDockerImage(def script, String image, Closure body) {
+  echo "${cesBuildLib}"
+  def docker = cesBuildLib.Docker.new(script)
+  echo "${docker}"
+  docker.image(image)
   // Allow accessing WORKSPACE even when we are in a child dir (using "dir() {}")
           .inside("${pwd().equals(env.WORKSPACE) ? '' : "-v ${env.WORKSPACE}:${env.WORKSPACE}"}") {
             body()
